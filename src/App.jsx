c44b846e81fb4b83bb9780e903062a3179c9aec7
catherine-anokwuru <catherineanokwuru@gmail.com>
2023-07-08 11:32:20 +0100

    m

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from "./pages/contact/Contact";
 
 const App = () => {
   return (
-    <HashRouter>
+    <HashRouter basename={import.meta.env.DEV ? "/" : "/safemain/"}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Carousel />} />
